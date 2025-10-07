d2bb46d1403ee64aaf6cf133888124406fcf7a54
GyanPosling <okicok578@gmail.com>
2025-10-07 17:56:45 +0300

    Initial commit

---
+++ infra/eurekaService/src/test/java/com/bank/eureka/eurekaService/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.eureka.eurekaService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ services/authService/src/test/java/com/bank/auth/authService/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.auth.authService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
